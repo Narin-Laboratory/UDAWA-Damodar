7b506a5dac1255e2d1fd67744043d051fc582b70
revivalxvr <revivaliskandar@gmail.com>
2024-02-26 21:42:44 +0800

    implementasi kalman filter untuk sensor TDS

--- firmware/damodar_nano/src/main.cpp
+++ firmware/damodar_nano/src/main.cpp
@@ -1,10 +1,20 @@
 #include "main.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
 
 libudawaatmega328 nano;
-
 Settings mySettings;
-
 Sensors sensors;
+
+OneWire oneWireDS18B20(mySettings.pinSuhuData);
+DallasTemperature ds18b20(&oneWireDS18B20);
+
+// Variabel-variabel untuk Filter Kalman
+float estimasi_ppm = 0; // Estimasi nilai ppm oleh filter Kalman
+float error_estimasi = 1; // Error estimasi (P)
+float error_pengukuran = 3; // Error pengukuran (R), asumsikan nilai ini atau sesuaikan berdasarkan kebisingan sensor
+float kalman_gain = 0; // Kalman gain (K) // Sensor noise. Adjust this value based on your sensor's performance.
 
 void setup() {
   // put your setup code here, to run once:
@@ -22,7 +32,7 @@ void loop() {
     const char* method = doc["method"].as<const char*>();
     if(strcmp(method, (const char*) "setSettings") == 0)
     {
-      if(doc["example"] != nullptr){mySettings.example = doc["example"].as<int>();}
+      // if(doc["example"] != nullptr){mySettings.example = doc["example"].as<int>();}
     }
     else if(strcmp(method, (const char*) "readSensors") == 0)
     {
@@ -36,8 +46,28 @@ void loop() {
 }
 
 void readSensors(){
-  sensors.ppm = rand();
-  sensors.cels = rand();
+  
+      // Membaca nilai sensor TDS
+    int sensorValueTds = analogRead(mySettings.pinTdsData);
 
-  mySettings.pinTdsData;
+    // Memperbarui estimasi dengan prediksi sebelumnya
+    float prediksi_ppm = estimasi_ppm; // Dalam kasus sederhana ini, modelnya adalah identitas
+
+    // Menghitung Kalman Gain
+    kalman_gain = error_estimasi / (error_estimasi + error_pengukuran);
+
+    // Memperbarui estimasi dengan pengukuran terkini
+    estimasi_ppm = prediksi_ppm + kalman_gain * (sensorValueTds - prediksi_ppm);
+
+    // Memperbarui error estimasi
+    error_estimasi = (1 - kalman_gain) * error_estimasi;
+
+    // Simpan nilai ppm yang difilter ke dalam struktur Sensors
+    sensors.ppm = estimasi_ppm;
+
+    ds18b20.requestTemperatures();  // Mengirim perintah untuk membaca suhu
+    float temperatureCelsius = ds18b20.getTempCByIndex(0);  // Mengambil nilai suhu dalam derajat Celsius
+
+    // Menyimpan nilai suhu ke dalam struktur Sensors
+    sensors.cels = temperatureCelsius;
 }
--- firmware/damodar_nano/src/main.h
+++ firmware/damodar_nano/src/main.h
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 #include <libudawaatmega328.h>
 
+
 struct Settings
 {
     int pinTdsData = A0;
+    int pinSuhuData = 4;
+    
 };
 
 struct Sensors
@@ -11,6 +14,5 @@ struct Sensors
    float ppm;
    float cels;
 };
-
 
 void readSensors();
